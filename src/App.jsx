e0903069461ca353d0c1935e54c9b97e3ae33a09
Bhupesh-Thakur <153527966+thakurbhupesh785@users.noreply.github.com>
2024-02-09 16:00:40 +0530

    Add files via upload
    just diff ways to add css in our react components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// How to style react components with CSS
+// excluding frameworks 
+
+// 1 external
+// 2 modules
+// 3 inline
+
+import Button from './Button.jsx'
+
+function App() {
+  
+  return(
+    <> 
+   <Button/>
+    </>
+  )
+ 
+}
+
+export default App
---
+++ src/Button.jsx
@@ -0,0 +1,18 @@
+
+function Button () {
+
+  const styles = {
+      backgroundColor:"rgb(13, 168, 229)",
+      color: "white",
+      padding: "10px 20px",
+      borderRadius: "5px",
+      border: "none",
+      cursor: "pointer",
+  }
+
+  return(
+    <button style={styles}>Click me</button>
+  )
+
+}
+export default Button
